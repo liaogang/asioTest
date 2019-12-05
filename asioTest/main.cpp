7aea3b299683f3423f55d29773dda2c135ef34a2
liaogang <ynwlgh@icloud.com>
2019-12-05 17:12:50 +0800

    update

--- asioTest/main.cpp
+++ asioTest/main.cpp
@@ -10,6 +10,7 @@
 #include <boost/asio.hpp>
 #include <memory>
 #include <vector>
+#include "rpccall.pb.h"
 
 using namespace boost;
 using boost::asio::ip::tcp;
@@ -27,19 +28,29 @@ public:
     {
         auto self(shared_from_this());
 
-        uint32_t raw_header;
-        auto buf = asio::buffer((void*)&raw_header, 4);
         
-        asio::async_read(socket_, buf, [this, self, raw_header](std::error_code ec, std::size_t length){
+        auto buf = asio::buffer((void*)&header_buf_, 4);
+        
+        asio::async_read(socket_, buf, [this, self](std::error_code ec, std::size_t length){
 
             printf("read header: %lu \n", length);
 
             if (ec) {
             }
             else{
-                //decode header
-                uint32_t body_length = ntohl(raw_header);
-                do_read_body(body_length);
+                
+                if (length == 4) {
+                    printf("read head ok: %d\n", length);
+                    
+                    
+                    //decode header
+                    uint32_t body_length = ntohl(this->header_buf_);
+                    do_read_body(body_length);
+                    
+                }else{
+                    printf("read head failed.\n");
+                }
+
             }
         });
         
@@ -47,34 +58,62 @@ public:
     
     void do_read_body(uint32_t body_length)
     {
+        printf("to read body of length: %d \n", body_length);
+        
         auto self(shared_from_this());
         
-        std::string str;
-        str.resize(body_length);
+        body_buf_.resize(body_length);
         
-        asio::async_read(socket_, asio::buffer(str), [this, self, str](boost::system::error_code ec, std::size_t length) {
+        asio::async_read(socket_, asio::buffer(body_buf_), [this, self](boost::system::error_code ec, std::size_t length) {
             
             if (ec) {
             }else{
-                this->do_read_rpc( str );
+                
+                if (length == this->body_buf_.length()) {
+                    printf("read body ok: %d\n", length);
+                    
+                    
+                    this->do_read_rpc( this->body_buf_ );
+                    this->body_buf_.clear();
+                    
+                }else{
+                    printf("read body failed.\n");
+                }
+
+                
             }
             
         });
         
     }
     
-    void do_read_rpc(std::string rpc)
+    void do_read_rpc(std::string& data)
     {
-     
-        
-        
+        if (deviceName_.empty()) {
+            
+            DeviceInfo deviceInfo;
+            deviceInfo.ParseFromString(data);
+
+            printf("接收到鉴定: %s \n",  deviceInfo.DebugString().c_str());
+            
+            deviceName_ = deviceInfo.devicename();
+        }
+        else{
+            RPCCall rpccall;
+            rpccall.ParseFromString(data);
+            
+            printf("接收到服务器消息: %s \n",  rpccall.DebugString().c_str() );
+        }
         
     }
     
     
 private:
     tcp::socket socket_;
-
+    std::string deviceName_;
+    
+    uint32_t header_buf_;
+    std::string body_buf_;
 };
 
 
